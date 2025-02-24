f248bcbcb126fb3076055d0ebc73c8f704aa275e
ShaunT-bit <tshaun114@gmail.com>
2025-02-25 00:11:48 +0400

    fixed desktop and tab view

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -3,7 +3,7 @@ import ArrowDown from "@/assets/icons/arrow-down.svg"
 import Image from "next/image"
 
 export const HeroSection = () => {
-  return <div className="py-32">
+  return <div className="py-32 md:py-48 lg:py-60">
     <div className="container">
       <div className="flex flex-col items-center ">
 
@@ -18,15 +18,21 @@ export const HeroSection = () => {
       </div>
 
 
-      <h1 className="font-serif text-3xl text-center mt-8 tracking-wide">Building Exceptional User Experiences</h1>
+      <div className="max-w-lg mx-auto">
+        <h1 className="font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide">Building Exceptional User
+          Experiences</h1>
 
-      <p className="mt-4 text-center text-white/60"> I specialize in transforming designs into functional,high-performing web applications. Let's discuss your next
-        project</p>
+        <p className="mt-4 text-center text-white/60 md:text-lg"> I specialize in transforming designs into
+          functional,high-performing web applications. Let's discuss your next
+          project</p>
 
-      <div className="flex flex-col items-center mt-8  gap-4">
+      </div>
+
+
+      <div className="flex flex-col md:flex-row justify-center items-center mt-8  gap-4">
         <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl ">
           <span className="font-semibold">Explore my work</span>
-          <ArrowDown className="size-4" />
+          <ArrowDown className="size-4"/>
         </button>
 
         <button className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 h-12 px-6 rounded-xl ">
