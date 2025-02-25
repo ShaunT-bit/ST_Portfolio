a691add7098d0da4749db3ff6486b153e6968416
ShaunT-bit <tshaun114@gmail.com>
2025-02-26 00:12:59 +0400

    finished with hero section

--- src/components/HeroOrbit.tsx
+++ src/components/HeroOrbit.tsx
@@ -1,13 +1,15 @@
 
 import {PropsWithChildren} from "react";
 
-export const HeroOrbit = ({children,size}:PropsWithChildren<{size:number}>) => {
+export const HeroOrbit = ({children,size,rotation}:PropsWithChildren<{size:number;rotation:number}>) => {
     return (
         <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-            <div className="border border-red-500 size-[800px]" style = {{
-                height:`${size}px`,width:`${size}px`
+            <div className="" style = {{
+                height:`${size}px`,
+                width:`${size}px`,
+                transform:`rotate(${rotation}deg)`,
             }}>
-                <div className="border border-red-500 inline-flex">
+                <div className=" inline-flex flex items-start justify-start " style = {{transform: `rotate(${rotation*-1}deg)`}}>
                     {children}
                 </div>
             </div>
